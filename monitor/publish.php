604e0f4eb021dd966dea9de37600b693ab7646d3
revaHanafi <108256414+revaHanafi@users.noreply.github.com>
2023-12-15 16:16:13 +0700

    Add files via upload

---
+++ monitor/publish.php
@@ -0,0 +1,29 @@
+<?php
+require("phpMQTT.php");
+require("config.php");
+$topic="iot/G.211.20.0012";
+
+$message = @$_GET['message'];
+
+if(empty($message)) { $message = "TEST"; }
+
+//MQTT client id to use for the device. "" will generate a client id automatically
+$mqtt = new bluerhinos\phpMQTT($host, $port, "G.211.20.0012-09930".rand());
+if ($mqtt->connect(true,NULL,$username,$password)) {
+  $mqtt->publish($topic,$message, 0);
+  $mqtt->close();
+}else{
+  echo "Fail or time out
+";
+}
+
+echo "[$topic] mengirim messages: $message ";
+
+
+
+?>
+
+<script>
+
+window.history.back();
+</script>
